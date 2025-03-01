7bb7d946226378f73648deb8f1686b42dbe3dfbc
Bihesha <bihesha0822@gmail.com>
2025-03-01 18:29:30 +0530

    updated the server.js file.

--- helpdesk-backend/server.js
+++ helpdesk-backend/server.js
@@ -1,19 +1,19 @@
 const express = require('express');
 const cors = require('cors');
+const app = express();
 require('dotenv').config();
 const connectToDatabase = require('./config/database');
+const { authenticate } = require("./middleware/authMiddleware");
 
 // Initializing the port number
 const port = process.env.PORT || 8070;
 
+// Middleware
+app.use(cors());
+app.use(express.json());
+
 // Connect to the database first, then start the server
 connectToDatabase(process.env.MONGODB_URL).then(() => {
-    const app = express();
-
-    // Middleware
-    app.use(cors());
-    app.use(express.json());
-
     // Server Connection
     app.listen(port, () => {
         console.log(`Server is up and running on port number ${port}`);
@@ -21,3 +21,19 @@ connectToDatabase(process.env.MONGODB_URL).then(() => {
 }).catch(err => {
     console.error("Failed to connect to the database. Server not started.");
 });
+
+// auth routes
+const authRouter = require('./routes/authRoutes');
+app.use('/auth',authRouter);
+
+// admin routes
+const adminRouter = require('./routes/adminRoutes');
+app.use('/admin', adminRouter);
+
+// student routes
+const studentRouter = require('./routes/studentRoutes');
+app.use('/student', authenticate, studentRouter);
+
+// role based authenticate
+const authenticateRole = require('./routes/authenticateRole');
+app.use('/authenticate-role', authenticateRole);
