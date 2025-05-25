6ebf0710bb5d951219d49781ff6a3ea2657e9d27
ManulaGunatilleke <inner.gunatilleke@gmail.com>
2025-05-25 16:58:53 +0530

    Update backend for docker internal host

--- helpdesk-backend/kafka/kafkaConfig.js
+++ helpdesk-backend/kafka/kafkaConfig.js
@@ -2,7 +2,7 @@ const { Kafka } = require('kafkajs');
 
 const kafka = new Kafka({
   clientId: 'helpdesk-backend',
-  brokers: ['localhost:9092']
+  brokers: ['host.docker.internal:9092']
 });
 
 const producer = kafka.producer();
